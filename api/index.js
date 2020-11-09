115204a587b5a9ddaf2858f5d2e52420c4ec94a5
Donnie An <acmilank22@gmail.com>
2020-11-09 09:53:53 +0800

    feat: adapt Vercel

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const index = express();
+const app = express();
 const port = 3000;
 
-index.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-index.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
